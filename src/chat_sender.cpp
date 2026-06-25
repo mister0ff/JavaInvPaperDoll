deae0aeef664312c535c08c5989f4a59afdc9ec5
Eduhh <frazaoe620@gmail.com>
2026-06-25 20:46:02 -0300

    Refactor chat message sending and remove library check
    Refactor chat sending logic to try multiple methods for sending chat messages. Removed unused library loading code.

--- src/chat_sender.cpp
+++ src/chat_sender.cpp
@@ -2,11 +2,9 @@
 #include <android/log.h>
 #include <dlfcn.h>
 #include <jni.h>
-#include <cstring>
 
 namespace {
 constexpr const char* kLogTag = "AutoGGButton";
-constexpr const char* kMinecraftLibrary = "libminecraftpe.so";
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, kLogTag, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, kLogTag, __VA_ARGS__)
 JavaVM* g_vm = nullptr;
@@ -14,15 +12,6 @@ JavaVM* g_vm = nullptr;
 
 namespace ChatSender {
 void SetJVM(JavaVM* vm) { g_vm = vm; }
-
-bool Initialize() {
-    void* mc = dlopen(kMinecraftLibrary, RTLD_NOW | RTLD_NOLOAD);
-    if (!mc) {
-        LOGE("libminecraftpe.so not loaded");
-        return false;
-    }
-    return true;
-}
 
 void Send(const std::string& message) {
     JNIEnv* env = nullptr;
@@ -38,26 +27,27 @@ void Send(const std::string& message) {
     if (!getInstance) { LOGE("getInstance not found"); return; }
     
     jobject activity = env->CallStaticObjectMethod(activityClass, getInstance);
-    if (!activity) { LOGE("Could not get MainActivity"); return; }
+    if (!activity) { LOGE("No activity instance"); return; }
     
-    jmethodID sendChat = env->GetMethodID(activityClass, "nativeSendChatMessage", "(Ljava/lang/String;)V");
-    if (sendChat) {
-        jstring jmsg = env->NewStringUTF(message.c_str());
-        env->CallVoidMethod(activity, sendChat, jmsg);
-        env->DeleteLocalRef(jmsg);
-        LOGI("Chat sent: %s", message.c_str());
-        return;
-    }
+    // Tenta vários métodos de envio de chat
+    const char* methods[] = {
+        "nativeSendChatMessage",
+        "sendChatMessage", 
+        "sendMessage",
+        nullptr
+    };
     
-    sendChat = env->GetMethodID(activityClass, "sendChatMessage", "(Ljava/lang/String;)V");
-    if (sendChat) {
-        jstring jmsg = env->NewStringUTF(message.c_str());
-        env->CallVoidMethod(activity, sendChat, jmsg);
-        env->DeleteLocalRef(jmsg);
-        LOGI("Chat sent (fallback): %s", message.c_str());
-        return;
+    for (int i = 0; methods[i]; ++i) {
+        jmethodID sendChat = env->GetMethodID(activityClass, methods[i], "(Ljava/lang/String;)V");
+        if (sendChat) {
+            jstring jmsg = env->NewStringUTF(message.c_str());
+            env->CallVoidMethod(activity, sendChat, jmsg);
+            env->DeleteLocalRef(jmsg);
+            LOGI("Chat sent: %s", message.c_str());
+            return;
+        }
     }
     
     LOGE("No chat send method found");
 }
-}
+} // namespace ChatSender
