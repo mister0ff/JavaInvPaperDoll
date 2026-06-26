ba674d4dedc478e37b103ca6eb788000be1cf563
Eduhh <frazaoe620@gmail.com>
2026-06-26 20:52:49 -0300

    Delete src directory

--- src/floating_button.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace FloatingButton {
-    void Start();
-    void Stop();
-    bool IsInsideButtonArea(float x, float y);
-    void HandleTouchDown(float x, float y);
-    void HandleTouchUp(float x, float y);
-}
--- src/render_overlay.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace RenderOverlay {
-    void Init(int width, int height);
-    void Render();
-    void SetButtonPressed(bool pressed);
-    bool IsInsideButton(float x, float y);
-    void SetScreenSize(int w, int h);
-}
